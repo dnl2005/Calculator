80b1b10f740fceb143357242deba9a5ef94dfb79
maggson <dieseverycingein@yandex.ru>
2025-09-29 15:20:07 +0300

    done

--- ClassLibrary/Class1.cs
+++ ClassLibrary/Class1.cs
@@ -1,11 +1,24 @@
 
+using System.Data;
+
 namespace ClassLibrary
 {
     public static class Calculator
     {
-        public Calculator(string expression)
-        {
+        /// <summary>
+        /// аналог функции eval для математических выражений
+        /// </summary>
+        /// <param name="expression">  выражение, с которым будет вестись работа</param>
+        /// <returns> возвращает объект, содержащий int или double</returns>
 
+        static object Calculate(string expression)
+        {
+            //.complete сам вычисляет все, крутой мужик
+
+            var result = new DataTable().Compute(expression, null);
+
+            // при принятии результата в переменную (?) необходимо работать через convert.
+            return result; 
         }
     }
 }
