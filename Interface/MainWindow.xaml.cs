b000d1022d22ebc9d435c267a79106e8112e8ed9
anton <antontkachenko399@gmail.com>
2025-09-29 15:27:16 +0300

    калькулятор

--- Interface/MainWindow.xaml.cs
+++ Interface/MainWindow.xaml.cs
@@ -20,5 +20,15 @@ namespace Interface
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
